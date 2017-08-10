349acea8708e2fbf4e5197971f5141bdeb50f655
William Reilly <wreilly2001@gmail.com>
2017-08-10 04:50:53 -0400

    Lesson 13. Misc.

--- public/script.js
+++ public/script.js
@@ -1,2 +1,16 @@
 console.log('It works.');
 console.log(Vue);
+
+new Vue({
+    el: '#app',
+    data: {
+        total: 0
+    },
+    methods: {
+        addItem: function() {
+            this.total += 9.99
+            console.log('in addItem this.total post ', this.total)
+        }
+    }
+})
+
