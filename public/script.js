e5e509348e0395bc8d2e216b1e00ecfbf7e6db68
William Reilly <wreilly2001@gmail.com>
2017-08-10 05:22:30 -0400

    L-17 Cart WR__ solution

--- public/script.js
+++ public/script.js
@@ -1,13 +1,18 @@
-console.log('It works.');
-console.log(Vue);
-
 new Vue({
     el: '#app',
     data: {
-        total: 0
+        total: 0,
+        items: [
+            { title: 'Item 1' },
+            { title: 'Item 2' },
+            { title: 'Item 3' }
+        ],
+        cartItems: [] // empty to begin
     },
     methods: {
-        addItem: function() {
+        addItem: function(passedInMyItem) {
+            console.log('We got: ', passedInMyItem.title)
+            this.cartItems.push(passedInMyItem)
             this.total += 9.99
             console.log('in addItem this.total post ', this.total)
         }
