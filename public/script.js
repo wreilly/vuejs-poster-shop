4f1389d154c798365d54ac4999e8c83b4181ce65
William Reilly <wreilly2001@gmail.com>
2017-08-10 08:17:03 -0400

    L-29 Cart done. Quantity buttons logic

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+var PRICE = 9.99 // const in ES6
+
 new Vue({
     el: '#app',
     data: {
@@ -7,9 +9,26 @@ new Vue({
             { id: 2, title: 'Item 2' },
             { id: 3, title: 'Item 3' }
         ],
-        cartItems: [] // empty to begin
+        cartItems: [] // empty to begin. qty, price additional properties in the cart.
     },
     methods: {
+        inc: function(cartItem) {
+            console.log('inc ? cartItem, cartItem.qty ', cartItem, cartItem.qty)
+          cartItem.qty += 1
+            console.log('inc ? cartItem.qty ', cartItem.qty)
+        },
+        dec: function(cartItem) {
+            // Don't allow going below 0! (nor "to zero")
+            if (cartItem.qty > 1) {
+                cartItem.qty -= 1
+            } else {
+                // if we get here it is 0 - do not display '0' to screen. just remove from cart
+                // console.log('You can\'t take one out, because right now there are 0 of this bad boy in cart ', cartItem.title)
+                var myIndex = this.cartItems.findIndex((it) => it.id === cartItem.id)
+                this.cartItems.splice(myIndex, 1)
+            }
+
+        },
         addItem: function(passedInMyIndex) {
             console.log('We got: -01- ', this.items[passedInMyIndex].title)
 
@@ -27,8 +46,23 @@ new Vue({
 
             if (foundCartItemIndex === -1) {
                 // Not found in cart. Add it!
+
+                /*
+                (RE)-Learning!!!
+                1. Do NOT just assign ('=') an Object onto a new variable.
+                Q. Why?
+                A. That new variable is pointing to the SAME Object. Not what you want.
+
+                2. Instead, use Object.assign() ...
+                 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+                 */
+
+/* Not What You Want:
                 var tempCartItemToAdd = this.items[passedInMyIndex]
+*/
+                var tempCartItemToAdd = Object.assign({}, this.items[passedInMyIndex]) // << YES.
                 tempCartItemToAdd.qty = 1 // Tack on a 'qty' property
+                tempCartItemToAdd.price = PRICE // Likewise a (hard-coded) price - same for all
                 this.cartItems.push(tempCartItemToAdd)
             } else {
                 this.cartItems[foundCartItemIndex].qty += 1
@@ -36,6 +70,23 @@ new Vue({
             this.total += 9.99
             console.log('in addItem this.total post ', this.total)
         }
+    },
+    filters: {
+        currency: function(price) {
+            return '$'.concat(price.toFixed(2))
+        }
+/*
+        // Who knew?
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/NumberFormat
+        var myFormatter = Intl.NumberFormat('en-us', {
+            style: 'currency',
+            currency: 'USD',
+            minimumFractionDigits: 2 // with : 2 get $123.80 << with : 0 I wasn't getting the final '0' in '.80'  - it was '.8' (not cool)
+        })
+
+        return myFormatter.format(priceNumber)
+*/
+
     }
 })
 
